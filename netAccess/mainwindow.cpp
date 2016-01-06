6a3cff07584dd9a730dbba42a758c304fd90053b
Karthik Nishanth <nishanthkarthik@live.com>
2016-01-06 19:07:22 +0530

    Hello world

---
+++ netAccess/mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "qmessagebox.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QPushButton* button = findChild<QPushButton*>("clickMeButton");
+    connect(button, SIGNAL (released()),this, SLOT (handleButton()));
+}
+
+int MainWindow::handleButton()
+{
+    QMessageBox messageBox;
+    messageBox.setText("Hello world from Qt");
+    qDebug("Button click logged");
+    int retCode = messageBox.exec();
+    return retCode;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
